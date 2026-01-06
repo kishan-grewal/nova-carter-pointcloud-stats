410b62a463113d3e843404b71ce266c7dcb99a57
Kishan Grewal <kishangrewal06@gmail.com>
2026-01-06 18:26:30 +0000

    add main.cpp

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
-# nova-carter-pointcloud-stats
-rclcpp ros2 system to analyse pointcloud and odom stats from a nova carter rosbag, recorded from isaac sim
+#nova - carter - pointcloud - stats
+rclcpp ros2 system to analyse pointcloud and odom stats from a nova carter
+    rosbag,
+    recorded from isaac sim
 
 ```bash
-# terminal 1
-source /opt/ros/humble/setup.zsh
-cd ~/ros2_ws
-source install/setup.zsh
-ros2 bag play ~/ros2_ws/bags/carter_cloud_odom_33s --loop
+#terminal 1
+                source /
+            opt / ros / humble / setup.zsh cd ~ / ros2_ws source install /
+            setup.zsh ros2 bag play ~ / ros2_ws / bags /
+            carter_cloud_odom_33s-- loop
 ```
 
 ```bash
-# terminal 2
-source /opt/ros/humble/setup.zsh
-cd ~/ros2_ws
-source install/setup.zsh
-colcon build --packages-select carter_bag_stats
-ros2 bag play ~/ros2_ws/bags/carter_cloud_odom_33s --loop
+#terminal 2
+                source /
+            opt / ros / humble / setup.zsh cd ~ / ros2_ws source install /
+            setup.zsh colcon build-- packages -
+        select carter_bag_stats ros2 bag play ~ / ros2_ws / bags /
+            carter_cloud_odom_33s-- loop
 ```
---
+++ carter_bag_stats/src/main.cpp
@@ -0,0 +1,35 @@
+#include <chrono>                     // std::chrono::milliseconds
+using namespace std::chrono_literals; // using namepsace -> convenient syntax
+// e.g. stop including (std) or use suffix like ms (chrono)
+
+#include <memory> // shared ptr
+#include <string> // std::string and to_string
+
+#include "main.hpp"
+#include "rclcpp/rclcpp.hpp" // node, init, spin, shutdown, RCLCPP_INFO (#define)
+#include "std_msgs/msg/string.hpp" // std_msgs::msg::String
+
+class MyPublisher : public rclcpp::Node {
+public:
+  MyPublisher() : Node("my_publisher") {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    timer_ = this->create_wall_timer(1000ms, [this]() { this->callback(); });
+  }
+
+private:
+  void callback() {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello World!";
+    RCLCPP_INFO(this->get_logger(), message.data.c_str());
+    publisher_->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+};
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv); // will always be like this
+  rclcpp::spin(std::make_shared<MyPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
