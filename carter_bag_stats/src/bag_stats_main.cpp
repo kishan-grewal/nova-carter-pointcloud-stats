5657c0afd947ae1a1e621c91786b1a4bb3d3b8fb
Kishan Grewal <kishangrewal06@gmail.com>
2026-01-07 21:15:07 +0000

    fix multithreaded exec

--- carter_bag_stats/src/bag_stats_main.cpp
+++ carter_bag_stats/src/bag_stats_main.cpp
@@ -7,7 +7,10 @@ int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   // rclcpp::executors::MultiThreadedExecutor
   rclcpp::executors::MultiThreadedExecutor executor(rclcpp::ExecutorOptions(), 2);
-  executor.add_node(std::make_shared<BagStats>());
+  // always keep node in a variable don't create it as an r-value in executor add node line
+  // otherwise its lifetime ends before the executor runs
+  auto node = std::make_shared<BagStats>();
+  executor.add_node(node);
   executor.spin();
   rclcpp::shutdown();
   return 0;
